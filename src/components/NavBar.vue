ded7d1e1991a420f8eecd944e5e971317ee8b7b7
Jenny Le <jennyle.dev@gmail.com>
2019-11-17 22:09:09 -0700

    Fixed FrogBother typo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar id="nav-bar" class="mb-4 pl-0 pr-0"> 
-      <b-nav-text><h6>Frog Bother Chat</h6></b-nav-text>
+      <b-nav-text><h6>FrogBother Chat</h6></b-nav-text>
       
       <b-navbar-brand class="ml-auto mr-0">
         <h4>
